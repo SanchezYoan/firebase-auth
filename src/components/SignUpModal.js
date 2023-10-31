931174b41f386a193b44eaac7436315e8f641d81
SanchezYoan <yoan30470@hotmail.fr>
2023-10-31 17:01:36 +0100

    userContext

--- src/components/SignUpModal.js
+++ src/components/SignUpModal.js
@@ -1,24 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
-const SignUpModal = () => {
+export default function SignUpModal() {
+  const { modalState, toggleModals } = useContext(UserContext);
+  console.log(modalState, toggleModals);
   return (
     <>
-      <div className="position-fixed top-0 vw-100 vh-100 ">
-        <div className="w-100 h-100 bg-dark bg-opacity-75 ">
+      {modalState.signUpModal && (
+        <div className="position-fixed top-0 vw-100 vh-100 ">
+          <div
+            className="w-100 h-100 bg-dark bg-opacity-75 "
+            onClick={() => toggleModals("close")}
+          >
+            {" "}
+          </div>
           <div
             className="position-absolute top-50 start-50 translate-middle "
             style={{ minWidth: "400px" }}
           >
             <div className="modal-dialog bg-white">
-              <div className="modal-content">
+              <div className="modal-content" style={{ padding: "10px" }}>
                 <div className="modal-header ">
                   <h5 className="modal-title">Sign Up</h5>
-                  <button className="btn-close"></button>
+                  <button
+                    onClick={() => toggleModals("close")}
+                    className="btn-close"
+                  ></button>
                 </div>
                 <div className="modal-body">
                   <form className="sign-up-from">
                     <div className="mb-3">
-                      <label htmlFor="signUpEmail">Email Adress</label>
+                      <label className="form-label" htmlFor="signUpEmail">
+                        Email Adress
+                      </label>
                       <input
                         name="email"
                         required
@@ -29,7 +43,9 @@ const SignUpModal = () => {
                     </div>
 
                     <div className="mb-3">
-                      <label htmlFor="repeatUpPwd">Password</label>
+                      <label className="form-label" htmlFor="repeatUpPwd">
+                        Password
+                      </label>
                       <input
                         name="pwd"
                         required
@@ -39,7 +55,9 @@ const SignUpModal = () => {
                       />
                     </div>
                     <div className="mb-3">
-                      <label htmlFor="repeatPwd">Repeat Password</label>
+                      <label className="form-label" htmlFor="repeatPwd">
+                        Repeat Password
+                      </label>
                       <input
                         name="pwd"
                         required
@@ -47,16 +65,17 @@ const SignUpModal = () => {
                         className="form-control"
                         id="repeatPwd"
                       />
+                      <p className="text-danger mt-1"></p>
                     </div>
+
+                    <button className="btn btn-primary">Submit</button>
                   </form>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </>
   );
-};
-
-export default SignUpModal;
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container p-5">
-      <h1 className="display-2 text-light">Hi, Sign Up or Sign in</h1>
+      <h1 className="display-3 text-light">Hi, Sign Up or Sign in</h1>
     </div>
   );
 };
